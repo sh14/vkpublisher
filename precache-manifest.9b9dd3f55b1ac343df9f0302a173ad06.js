7265cde0aa80cacc0d2a786721a904dee5635731
sh14 <sh-v@ya.ru>
2019-07-28 11:36:15 +0300

    Updates

---
+++ precache-manifest.9b9dd3f55b1ac343df9f0302a173ad06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkpublisher/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "05739e58869089ed2725",
+    "url": "/vkpublisher/static/js/runtime~main.cec62944.js"
+  },
+  {
+    "revision": "bdbcd400bedb6b44d768",
+    "url": "/vkpublisher/static/js/main.4768e249.chunk.js"
+  },
+  {
+    "revision": "83fd625b23f8236c4b1e",
+    "url": "/vkpublisher/static/js/2.bfc5c423.chunk.js"
+  },
+  {
+    "revision": "bdbcd400bedb6b44d768",
+    "url": "/vkpublisher/static/css/main.a4aafc24.chunk.css"
+  },
+  {
+    "revision": "83fd625b23f8236c4b1e",
+    "url": "/vkpublisher/static/css/2.53816a50.chunk.css"
+  },
+  {
+    "revision": "c25d58fc8690c62ba2cba081165023e7",
+    "url": "/vkpublisher/index.html"
+  }
+];
